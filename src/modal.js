f2c39f81c9233e7fae4a50b4b95cdefcd102739d
Egor Malinin <malinin-2000@mail.ru>
2022-10-06 00:09:18 +0500

    Task landing page

---
+++ src/modal.js
@@ -0,0 +1,25 @@
+
+export const modal = document.getElementById("modal-form")
+const button = document.getElementById("work-with-us__button")
+const close = document.getElementsByClassName("modal-form__close")[0]
+const body = document.querySelector("body")
+
+const openModal = () => {
+    modal.style.display = "block"
+    body.style.overflow = "hidden"
+}
+
+const closeModalOnBtn = () => {
+    modal.style.display = "none"
+    body.style.overflow = "visible"
+}
+
+const closeModalOnSpace = (ev) => {
+    if (ev.target === modal) modal.style.display = "none"
+    if (modal.style.display === "none") body.style.overflow = "visible"
+}
+
+button.onclick = () => openModal()
+close.onclick = () => closeModalOnBtn()
+modal.onclick = (ev) => closeModalOnSpace(ev)
+
